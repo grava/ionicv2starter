f24cc13d6cf27e1ed23c835424a18076adbce6e4
Gianluca Gravina <gianluca.gravina@gmail.com>
2016-08-11 17:34:45 +0200

    first commit

---
+++ app/pages/hello-ionic/hello-ionic-page.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component( {
+    templateUrl: 'build/pages/hello-ionic/hello-ionic-page.html'
+})
+
+export class HelloIonicPage{
+    constructor(){}
+}
